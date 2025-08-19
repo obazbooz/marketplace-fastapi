250baa39d2fc33192b5f0bd6967866089d5f1b22
Amirhossein Farahaniolia <amirolia@AmirhosseinsMBP.localdomain>
2025-08-19 09:04:49 +0200

    Update modified files

--- router/user.py
+++ router/user.py
@@ -20,25 +20,25 @@ def create_user(request: UserBase, db: Session = Depends(get_db)):
 
 
 # ✅ Read all users
-@router.get('/', response_model=List[UserDisplay], status_code=status.HTTP_200_OK)
-def get_all_users(db: Session = Depends(get_db)):
-    return db_user.get_all_users(db)
+#@router.get('/', response_model=List[UserDisplay], status_code=status.HTTP_200_OK)
+#def get_all_users(db: Session = Depends(get_db)):
+#    return db_user.get_all_users(db)
 
 
 # ✅ Read one user by ID
-@router.get('/{id}', response_model=UserDisplay, status_code=status.HTTP_200_OK)
-def get_user(id: int, db: Session = Depends(get_db)):
-    return db_user.get_user(db, id)
+#@router.get('/{id}', response_model=UserDisplay, status_code=status.HTTP_200_OK)
+#def get_user(id: int, db: Session = Depends(get_db)):
+#    return db_user.get_user(db, id)
 
 
 # ✅ Update user
-@router.put('/{id}', response_model=UserDisplay, status_code=status.HTTP_200_OK)
-def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
-    return db_user.update_user(db, id, request)
+#@router.put('/{id}', response_model=UserDisplay, status_code=status.HTTP_200_OK)
+#def update_user(id: int, request: UserBase, db: Session = Depends(get_db)):
+#    return db_user.update_user(db, id, request)
 
 
 # ✅ Delete user
-@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
-def delete_user(id: int, db: Session = Depends(get_db)):
-    db_user.delete_user(db, id)
-    return  
+#@router.delete('/{id}', status_code=status.HTTP_204_NO_CONTENT)
+#def delete_user(id: int, db: Session = Depends(get_db)):
+#    db_user.delete_user(db, id)
+#    return  
